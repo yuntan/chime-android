9ccb70f0283f99c62dd3f53ab3677ec072ae3bdd
Yuto Tokunaga <yuntan.sub1@gmail.com>
2018-11-05 00:01:07 +0900

    initial commit

---
+++ app/src/main/java/xyz/untan/chime/MainActivity.kt
@@ -0,0 +1,12 @@
+package xyz.untan.chime
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
