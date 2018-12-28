5e684220fa21d995e253f0c37ae8cd8991611da1
Yuto Tokunaga <yuntan.sub1@gmail.com>
2018-12-28 20:12:45 +0900

    設定画面を追加，不要なActivityを削除

---
+++ app/src/main/java/xyz/untan/chime/SettingsFragment.kt
@@ -0,0 +1,42 @@
+package xyz.untan.chime
+
+import android.os.Bundle
+import android.support.v7.preference.PreferenceFragmentCompat
+import android.util.Log
+
+class SettingsFragment : PreferenceFragmentCompat() {
+    private val TAG: String = SettingsFragment::class.java.simpleName
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        Log.d(TAG, "onCreatePreferences")
+
+        addPreferencesFromResource(R.xml.pref_settings)
+//            setHasOptionsMenu(true)
+
+        // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+        // to their values. When their values change, their summaries are
+        // updated to reflect the new value, per the Android Design
+        // guidelines.
+//            bindPreferenceSummaryToValue(findPreference("sound_devices"))
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        Log.d(tag, "onActivityCreated")
+        super.onActivityCreated(savedInstanceState)
+
+        val btnTest = findPreference("test")
+        btnTest.setOnPreferenceClickListener {
+            // TODO
+            true
+        }
+    }
+
+//        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//            val id = item.itemId
+//            if (id == android.R.id.home) {
+//                startActivity(Intent(activity, SettingsActivity::class.java))
+//                return true
+//            }
+//            return super.onOptionsItemSelected(item)
+//        }
+}
